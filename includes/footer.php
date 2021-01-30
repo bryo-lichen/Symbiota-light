94d37a892b7664d4e603163c52ddc20e921f91d7
Edward Gilbert <egbot@asu.edu>
2021-01-29 17:21:17 -0700

    Add configuration files

---
+++ includes/footer.php
@@ -0,0 +1,15 @@
+    	</td>
+	</tr>
+	<tr>
+		<td id="footer"  colspan="3">
+			<div style="padding-top:50px;text-align:center;margin-left:auto;margin-right:auto">
+                                <a href="https://www.nsf.gov" target="_blank"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo_nsf.gif" style="height:75px;margin-right:200px" title="National Science Foundation" /></a>
+ 				<a href="https://www.idigbio.org" target="_blank"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo_idig.png" style="width:175px" /></a>
+                        </div>
+			<div style="padding:20px;text-align:center;margin-left:auto;margin-right:auto">
+				This project made possible by National Science Foundation Awards: 
+				<a href="https://www.nsf.gov/awardsearch/showAward?AWD_ID=1115116" target="_blank">#1115116</a>
+			</div>
+		</td>
+	</tr>
+</table>
