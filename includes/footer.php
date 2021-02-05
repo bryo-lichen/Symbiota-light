0e268edd2439a200adca6d6c3b5455810f198380
Edward Gilbert <egbot@asu.edu>
2021-02-05 10:36:30 -0700

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -7,7 +7,7 @@
  				<a href="https://www.idigbio.org" target="_blank"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo_idig.png" style="width:175px" /></a>
                         </div>
 			<div style="padding:20px;text-align:center;margin-left:auto;margin-right:auto">
-				This project made possible by National Science Foundation Awards: 
+				This project made possible by National Science Foundation Award: 
 				<a href="https://www.nsf.gov/awardsearch/showAward?AWD_ID=1115116" target="_blank">#1115116</a>
 			</div>
 		</td>
