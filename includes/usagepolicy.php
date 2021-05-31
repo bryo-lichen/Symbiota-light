e34efdb821da382022e27bf030cc67e5e67692cf
Katie Pearson <katelin.d.pearson24@gmail.com>
2021-05-31 14:58:15 -0700

    Update usagepolicy.php

--- includes/usagepolicy.php
+++ includes/usagepolicy.php
@@ -146,7 +146,8 @@
 				</div>
 				<div style="margin:10px;">
 					<?php 
-					//echo 'Consorcio de Herbarios de Líquenes en América Latina (CHLAL):'. '.date('Y').'. '; 
+					echo 'Consorcio de Herbarios de Líquenes en América Latina (CHLAL):'; 
+					echo '.date('Y')' . '. ';
 					echo 'http//:'.$_SERVER['HTTP_HOST'].$CLIENT_ROOT.(substr($CLIENT_ROOT,-1)=='/'?'':'/').'index.php. '; 
 					echo 'Fecha de acceso: '.date('d m Y').'. ';
 					?>
