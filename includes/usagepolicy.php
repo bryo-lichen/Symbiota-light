6ddf50d057c433b980d96fe2b7b7e0187c218b25
Katie Pearson <katelin.d.pearson24@gmail.com>
2021-05-31 14:54:48 -0700

    Update usagepolicy.php

--- includes/usagepolicy.php
+++ includes/usagepolicy.php
@@ -145,6 +145,13 @@
 					Uso de datos de ocurrencia para instituciones específicas:
 				</div>
 				<div style="margin:10px;">
+					<?php 
+					/*echo 'Consorcio de Herbarios de Líquenes en América Latina (CHLAL):'. '.date('Y').'. '; 
+					echo 'http//:'.$_SERVER['HTTP_HOST'].$CLIENT_ROOT.(substr($CLIENT_ROOT,-1)=='/'?'':'/').'index.php. '; 
+					echo 'Fecha de acceso: '.date('d m Y').'. '; 
+					?>
+				</div>
+				<div style="margin:10px;">
 					Datos de biodiversidad de ocurrencias de especímenes publicado por &lt;listado de colecciones&gt;
 					(obtenido de <?php echo $DEFAULT_TITLE; ?>, 
 					<?php echo 'http//:'.$_SERVER['HTTP_HOST'].$CLIENT_ROOT.(substr($CLIENT_ROOT,-1)=='/'?'':'/').'index.php'; ?>, DD-MM-YYYY)<br/><br/>
