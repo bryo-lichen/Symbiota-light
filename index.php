f2c69fd719dca682804f33591cc0378a27c2021c
Katie Pearson <katelin.d.pearson24@gmail.com>
2022-02-22 15:52:09 -0800

    Change CHLAL quick search to same format as CNALH

--- index.php
+++ index.php
@@ -26,13 +26,17 @@ header("Content-Type: text/html; charset=".$CHARSET);
 	<!-- This is inner text! -->
 	<div  id="innertext">
 		<div style="float:right;margin-left:15px;">
-			<div id="quicksearchdiv">
-				<!-- -------------------------QUICK SEARCH SETTINGS--------------------------------------- -->
-				<form name="quicksearch" id="quicksearch" action="<?php echo $CLIENT_ROOT; ?>/taxa/index.php" method="get" onsubmit="return verifyQuickSearch(this);">
-					<div id="quicksearchtext" ><?php echo (isset($LANG['QSEARCH_SEARCH'])?$LANG['QSEARCH_SEARCH']:'Search Taxon'); ?></div>
-					<input id="taxa" type="text" name="taxon" />
-					<button name="formsubmit"  id="quicksearchbutton" type="submit" value="Search Terms"><?php echo (isset($LANG['QSEARCH_SEARCH_BUTTON'])?$LANG['QSEARCH_SEARCH_BUTTON']:'Search'); ?></button>
-				</form>
+			<div id="quicksearchdiv" style="width:400px;border:#000000 solid 0px;">
+				<div style="margin-left:auto;margin-right:auto;width:360px">
+					<!-- -------------------------QUICK SEARCH SETTINGS--------------------------------------- -->
+					<form name="quicksearch" id="quicksearch" action="<?php echo $CLIENT_ROOT; ?>/taxa/index.php" method="get" onsubmit="return verifyQuickSearch(this);">
+						<div id="quicksearchtext" ><?php echo (isset($LANG['QSEARCH_SEARCH'])?$LANG['QSEARCH_SEARCH']:'Search Taxon'); ?></div>
+						<input id="taxa" type="text" name="taxon" style="width:275px" />
+						<button name="formsubmit"  id="quicksearchbutton" type="submit" value="Search Terms">
+							<?php echo (isset($LANG['QSEARCH_SEARCH_BUTTON'])?$LANG['QSEARCH_SEARCH_BUTTON']:'Search'); ?>
+						</button>
+					</form>
+				</div>
 			</div>
 			<div style="">
 				<?php
