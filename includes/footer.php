912962689dd677b56e3020965185023cf5f00bf9
Katie Pearson <katelin.d.pearson24@gmail.com>
2022-03-17 13:29:12 -0700

    Add GLOBAL logo to footer

--- includes/footer.php
+++ includes/footer.php
@@ -7,6 +7,7 @@
 				<a href="https://biokic.asu.edu/lichen-herbarium" target="_blank" title="Arizona State University Lichen Herbarium"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/ASU-LichenLogo.jpg" /></a>
 				<a href="http://glalia.blogspot.com/" target="_blank" title="Grupo Latinoamericano de Liquen&oacute;logos"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/GLALIA-Logo.jpg" style="width:120px" /></a>
 				<img src="<?php echo $CLIENT_ROOT; ?>/images/layout/GEL-logo.jpg" style="width:115px" title="Grupo Ecuatoriano de Liquenolog&iacute;a (GEL)" />
+				<a href="https://globaltcn.utk.edu/" targe="_blank"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/global_logo.png" style="width:150px" /></a>
 				<!--
 				<a href="http://www.darwinfoundation.org/datazone/" target="_blank" title=""><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/CDF-logo.jpg" style="width:80px" /></a>
 				 -->
