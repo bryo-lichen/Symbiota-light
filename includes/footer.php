900124fdc5f967b956ce430ca1b338c7632f27d5
Katie Pearson <katelin.d.pearson24@gmail.com>
2023-01-03 11:32:30 -0800

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -4,7 +4,6 @@
 		<td id="footer"  colspan="3">
 			<p style="margin:20px 100px">
 				<a href="https://biokic.asu.edu" target="_blank" title="Biodiversity Knowledge Integration Center"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo-asu-biokic.png" style="width:180px" /></a>
-				//<a href="https://biokic.asu.edu/lichen-herbarium" target="_blank" title="Arizona State University Lichen Herbarium"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo_asu-lichen.jpg" /></a>
 				<a href="https://www.iucnredlist.org/" target="_blank" title="IUCN"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/1521112071-iucn.png" style="width:70px" /></a>
 				<a href="http://idigbio.org" target="_blank" title="iDigBio"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo_idig.png" style="width:150px" /></a>
 				<a href="https://www.nsf.gov" target="_blank"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo_nsf.gif" style="width:70px" /></a>
