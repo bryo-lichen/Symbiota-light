0db8eee388b3485927c7748c1ecd88178ecbf08f
Katie Pearson <katelin.d.pearson24@gmail.com>
2023-03-16 11:24:53 -0700

    Add lang files

---
+++ content/lang/header.es.php
@@ -0,0 +1,71 @@
+<?php
+/*
+------------------
+Language: Español (Spanish)
+------------------
+*/
+
+$LANG['HEADER2'] = 'Consorcio de Herbarios de Bri&oacute;fitas';
+$LANG['HEADER3'] = '- construyendo un Consorcio de Briofitas y L&aacute;quenes: organismos clave de las comunidades criotobi&oacute;ticas.';
+
+$LANG['H_HOME'] = 'Inicio';
+
+$LANG['H_SEARCH'] = 'B&uacute;squeda';
+$LANG['H_COLLECTIONS'] = 'Buscar Colecciones';
+$LANG['H_MAP'] = 'B&uacute;squeda en Mapa';
+$LANG['H_EXSICCATAE'] = 'Exsiccatae';
+$LANG['H_DYN_LISTS'] = 'Listados Din&aacute;micos';
+$LANG['H_DYN_KEY'] = 'Clave de Identificaci&oacute;n Din&aacute;mica';
+$LANG['H_TAXONOMIC_EXPLORER'] = 'Explorador Taxon&oacute;mico';
+
+$LANG['H_IMAGES'] = 'Im&aacute;genes';
+$LANG['H_IMAGE_BROWSER'] = 'Navegador de Im&aacute;genes';
+$LANG['H_IMAGE_SEARCH'] = 'B&uacute;squeda de Im&aacute;genes';
+
+$LANG['H_INVENTORIES'] = 'Listas de Especies';
+$LANG['H_NORTH_AMERICA'] = 'Norteam&eacute;rica';
+$LANG['H_US'] = 'Estados Unidos';
+$LANG['H_AZ'] = 'Arizona';
+$LANG['H_CA'] = 'California';
+$LANG['H_IL'] = 'Illinois';
+$LANG['H_IO'] = 'Iowa';
+$LANG['H_ME'] = 'Maine';
+$LANG['H_MO'] = 'Misuri';
+$LANG['H_MT'] = 'Montana';
+$LANG['H_NM'] = 'Nuevo Mexico';
+$LANG['H_NY'] = 'Nuevo York';
+$LANG['H_NC'] = 'Carolina del Norte';
+$LANG['H_OH'] = 'Ohio';
+$LANG['H_PE'] = 'Pensilvania';
+$LANG['H_WA'] = 'Washington';
+$LANG['H_WY'] = 'Wyoming';
+
+$LANG['H_BEYOND_NA'] = 'M&aacute;s All&aacute; de Norteam&eacute;rica';
+$LANG['H_CHILE'] = 'Chile';
+$LANG['H_FALKLAND_ISL'] = 'Islas Malvinas';
+$LANG['H_FIJI'] = 'Fiyi';
+$LANG['H_GUATEMALA'] = 'Guatemala';
+$LANG['H_INDONESIA'] = 'Indonesia';
+$LANG['H_MALAYSIA'] = 'Malasia';
+
+$LANG['H_SP_GROUPS'] = 'Grupos de Especies';
+$LANG['H_US_NPS'] = 'Parques Nacionales de los EE.UU';
+$LANG['CROWDSOURCE'] = 'Crowdsourcing';
+$LANG['H_ASSOC_PROJECTS'] = 'Proyectos Asociados';
+$LANG['H_CON_LICHENS'] = 'Consorcio de Herbarios de L&iacute;quenes';
+$LANG['H_GLOBAL'] = 'GLOBAL Red de Bri&oacutefitos y L&iacute;quenes';
+
+$LANG['H_MORE_INFO'] = 'M&aacute;s Informaci&oacute;n';
+$LANG['H_DATA_POLICY'] = 'Política de Uso de Datos';
+$LANG['H_PARTNERS'] = 'Socios';
+$LANG['H_SITEMAP'] = 'Mapa del Sitio';
+$LANG['H_HELP'] = 'Ayuda';
+$LANG['H_SYMB_HELP'] = 'Ayuda de Symbiota';
+$LANG['CONSORTIUM_RESOURCES'] = 'Recursos del Consorcio de L&iacute;quenes';
+
+$LANG['H_WELCOME'] = 'Bienvenido';
+$LANG['H_MY_PROFILE'] = 'Mi Perfil';
+$LANG['H_LOGOUT'] = 'Finalizar Sesi&oacute;n';
+$LANG['H_LOGIN'] = 'Iniciar Sesi&oacute;n';
+$LANG['H_NEW_ACCOUNT'] = 'Nueva Cuenta';
+?>
---
+++ content/lang/header.fr.php
@@ -0,0 +1,71 @@
+<?php
+/*
+------------------
+Language: Français (French)
+------------------
+*/
+
+$LANG['HEADER2'] = 'Consortium des Herbiers de Bryophytes';
+$LANG['HEADER3'] = '- construire un consortium de bryophytes et de lichens comme cl&acute;s de vo&ucirc;te des communaut&eacute;s cryptobiotiques -';
+
+$LANG['H_HOME'] = 'Accueil';
+
+$LANG['H_SEARCH'] = 'Rechercher';
+$LANG['H_COLLECTIONS'] = 'Rechercher des Collections';
+$LANG['H_MAP'] = 'Recherche par Carte';
+$LANG['H_EXSICCATAE'] = 'Exsiccatae';
+$LANG['H_DYN_LISTS'] = 'Listes Dynamiques';
+$LANG['H_DYN_KEY'] = "Cl&eacute; d'Identification Dynamique";
+$LANG['H_TAXONOMIC_EXPLORER'] = 'Explorateur Taxonomique';
+
+$LANG['H_IMAGES'] = 'Images';
+$LANG['H_IMAGE_BROWSER'] = 'Vavigateur d\'Images';
+$LANG['H_IMAGE_SEARCH'] = 'Rechercher d\'Images';
+
+$LANG['H_INVENTORIES'] = 'Listes des Esp&egrave;ces';
+$LANG['H_NORTH_AMERICA'] = 'Am&eacute;rique du Nord';
+$LANG['H_US'] = '&Eacute;tats-Unis';
+$LANG['H_AZ'] = 'Arizona';
+$LANG['H_CA'] = 'Californie';
+$LANG['H_IL'] = 'Illinois';
+$LANG['H_IO'] = 'Iowa';
+$LANG['H_ME'] = 'Maine';
+$LANG['H_MO'] = 'Missouri';
+$LANG['H_MT'] = 'Montana';
+$LANG['H_NM'] = 'Nouveau Mexique';
+$LANG['H_NY'] = 'New York';
+$LANG['H_NC'] = 'Caroline du Nord';
+$LANG['H_OH'] = 'Ohio';
+$LANG['H_PE'] = 'Pennsylvanie';
+$LANG['H_WA'] = 'Washington';
+$LANG['H_WY'] = 'Wyoming';
+
+$LANG['H_BEYOND_NA'] = 'Au-del&agrave; de l\'Am&eacute;rique du Nord';
+$LANG['H_CHILE'] = 'Chili';
+$LANG['H_FALKLAND_ISL'] = '&Icirc;les Falkland';
+$LANG['H_FIJI'] = 'Fidji';
+$LANG['H_GUATEMALA'] = 'Guatemala';
+$LANG['H_INDONESIA'] = 'Indon&eacute;sie';
+$LANG['H_MALAYSIA'] = 'Malaisie';
+
+$LANG['H_SP_GROUPS'] = 'Groupes d\'Esp&egrave;ces';
+$LANG['H_US_NPS'] = 'Parcs Nationaux Am&eacute;ricains';
+$LANG['CROWDSOURCE'] = 'Crowdsourcing';
+$LANG['H_ASSOC_PROJECTS'] = 'Projets Associ&eacute;s';
+$LANG['H_CON_LICHENS'] = 'Consortium des Herbiers &agrave; Lichens';
+$LANG['H_GLOBAL'] = 'GLOBAL R&eacute;seau Bryophytes et Lichens';
+
+$LANG['H_MORE_INFO'] = 'Plus d\'Information';
+$LANG['H_DATA_POLICY'] = 'Politique d\'Utilisation des Donn&eacute;es';
+$LANG['H_PARTNERS'] = 'Partenaires';
+$LANG['H_SITEMAP'] = 'Plan du Site';
+$LANG['H_HELP'] = 'Assistance';
+$LANG['H_SYMB_HELP'] = 'Assistance de Symbiota';
+$LANG['CONSORTIUM_RESOURCES'] = 'Ressources du Consortium sur les Lichens';
+
+$LANG['H_WELCOME'] = 'Bienvenu';
+$LANG['H_MY_PROFILE'] = 'Mon Profill';
+$LANG['H_LOGOUT'] = 'Se Déconnecter';
+$LANG['H_LOGIN'] = 'Se connecter';
+$LANG['H_NEW_ACCOUNT'] = 'Nouveau Compte';
+?>
