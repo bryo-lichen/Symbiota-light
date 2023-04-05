d38644a0330ae1f99e277fc162d154b20ebc2681
Edward Gilbert <egbot@asu.edu>
2023-04-05 14:21:24 -0700

    Update head.php

--- includes/head.php
+++ includes/head.php
@@ -12,8 +12,8 @@
 ?>
 <meta name="viewport" content="initial-scale=1.0, user-scalable=yes" />
 
-<link href="<?php echo $CLIENT_ROOT; ?>/css/jquery-ui.css" type="text/css" rel="stylesheet">
-
 <link href="<?php echo $CLIENT_ROOT; ?>/css/base.css?ver=1" type="text/css" rel="stylesheet">
 <link href="<?php echo $CLIENT_ROOT; ?>/css/symb/custom/lbcc.css?ver=1" type="text/css" rel="stylesheet">
 <link href="<?php echo $CLIENT_ROOT; ?>/css/symb/custom/lbcc_header.css?ver=1" type="text/css" rel="stylesheet">
+
+<script src="<?php echo $CLIENT_ROOT; ?>/js/symb/lang.js" type="text/javascript"></script>
