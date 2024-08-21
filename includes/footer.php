d566e6ca3ac9a5a964f0e18449369f98fe394775
Greg Post <gregory.post@asu.edu>
2024-08-21 10:31:04 -0500

    Update refactor
    refactor head, footer.

--- includes/footer.php
+++ includes/footer.php
@@ -1,53 +1,34 @@
 <footer>
-	<!--
-	<dialog id="accessibility-modal" class="accessibility-dialog" aria-label="<?= $LANG['F_ACCESSIBILITY_OPTIONS']; ?>">
-		<h1><?= $LANG['F_ACCESSIBILITY_OPTIONS']; ?></h1>
-		<p class="bottom-breathing-room-rel"><?= $LANG['F_ACCESSIBILITY_OPTIONS_DESCRIPTION']; ?></p>
-		<button type="button" class="btn btn-primary bottom-breathing-room-rel" onclick="toggleAccessibilityStyles('<?php echo $CLIENT_ROOT . '/includes' . '/' ?>', '<?php echo $CSS_BASE_PATH ?>', '<?php echo $LANG['F_TOGGLE_508_OFF'] ?>', '<?php echo $LANG['F_TOGGLE_508_ON'] ?>')" id="accessibility-button" data-accessibility="accessibility-button">
-			<?= $LANG['F_TOGGLE_508_ON'] ?>
-		</button>
-		<form method="dialog">
-			<button type="submit" class="btn btn-primary"><?= $LANG['F_CLOSE']; ?></button>
-		</form>
-	</dialog>
-	-->
 	<div class="logo-gallery">
-		<!---
-		<button id="accessibility-options-button" type="button" class="btn btn-primary  accessibility-option-button">
-			<span class="button__item-container">
-				<?= $LANG['F_ACCESSIBILITY_OPTIONS']; ?>
-				<span>
-					<img alt="<?= $LANG['F_ACCESSIBILITY_ICON'] ?>" src="<?= $CLIENT_ROOT ?>/images/accessibility_FILL0_wght400_GRAD0_opsz24.svg" />
-				</span>
-	        </span>
-		</button>
-		--->
+		<?php
+		//include($SERVER_ROOT . '/accessibility/module.php');
+		?>
 		<a href="https://biokic.asu.edu" target="_blank" title="Biodiversity Knowledge Integration Center" aria-label="Visit BioKIC website">
-			<img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo-asu-biokic.png"  alt="Logo for the Biodiversity Knowledge Integration Center" />
+			<img src="<?= $CLIENT_ROOT; ?>/images/layout/logo-asu-biokic.png"  alt="Logo for the Biodiversity Knowledge Integration Center" />
 		</a>
 		<a href="https://biokic.asu.edu/lichen-herbarium" target="_blank" title="ASU Lichen Herbarium" aria-label="Visit ASU Lichen Herbarium">
-			<img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo_asu-lichen.jpg" alt="Logo ASU Lichen Herbarium"/>
+			<img src="<?= $CLIENT_ROOT; ?>/images/layout/logo_asu-lichen.jpg" alt="Logo ASU Lichen Herbarium"/>
 		</a>
 		<a href="https://www.iucnredlist.org/" target="_blank" title="IUCN" aria-label="Visit IUCN">
-			<img src="<?php echo $CLIENT_ROOT; ?>/images/layout/1521112071-iucn.png" alt="Logo for IUCN" />
+			<img src="<?= $CLIENT_ROOT; ?>/images/layout/1521112071-iucn.png" alt="Logo for IUCN" />
 		</a>
 		<a href="http://idigbio.org" target="_blank" title="iDigBio" aria-label="Visit iDigBio">
-			<img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo_idig.png" alt="Logo for iDigBio, or, Integrated Digitized Biocollections"/>
+			<img src="<?= $CLIENT_ROOT; ?>/images/layout/logo_idig.png" alt="Logo for iDigBio, or, Integrated Digitized Biocollections"/>
 		</a>
 		<a href="https://www.nsf.gov" target="_blank" aria-label="Visit National Science Foundation website">
-			<img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo_nsf.gif" alt="Logo for the National Science Foundation" />
+			<img src="<?= $CLIENT_ROOT; ?>/images/layout/logo_nsf.gif" alt="Logo for the National Science Foundation" />
 		</a>
-		<a href="https://globaltcn.utk.edu/" targe="_blank"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/global_logo.png" style="width:150px" /></a>
+		<a href="https://globaltcn.utk.edu/" targe="_blank"><img src="<?= $CLIENT_ROOT; ?>/images/layout/global_logo.png" style="width:150px" /></a>
 		<a href="http://glalia.blogspot.com/" target="_blank" title="Grupo Latinoamericano de Liquen&oacute;logos">
-			<img src="<?php echo $CLIENT_ROOT; ?>/images/layout/GLALIA-Logo.jpg" style="width:120px" />
+			<img src="<?= $CLIENT_ROOT; ?>/images/layout/GLALIA-Logo.jpg" style="width:120px" />
 		</a>
 		<a href="#">
-			<img src="<?php echo $CLIENT_ROOT; ?>/images/layout/GEL-logo.jpg" style="width:115px" title="Grupo Ecuatoriano de Liquenolog&iacute;a (GEL)" />
+			<img src="<?= $CLIENT_ROOT; ?>/images/layout/GEL-logo.jpg" style="width:115px" title="Grupo Ecuatoriano de Liquenolog&iacute;a (GEL)" />
 		</a>
 		
 	</div>
 	<p>
-		This project made possible by National Science Foundation Awards
+		<?= $LANG['F_NSF_AWARDS'] ?>
 		<a href="https://www.nsf.gov/awardsearch/showAward?AWD_ID=1115116" target="_blank">#1115116</a>,
 		<a href="https://www.nsf.gov/awardsearch/showAward?AWD_ID=2001500" target="_blank">#2001500</a>,
 		<a href="https://www.nsf.gov/awardsearch/showAward?AWD_ID=2001394" target="_blank">#2001394</a>
@@ -60,12 +41,4 @@
 	<p>
 	<?= $LANG['F_POWERED_BY'] ?> <a href="https://symbiota.org/" target="_blank">Symbiota</a>.
 	</p>
-	<!---
-	<script>
-		let toggleOff508 = "<?= $LANG['F_TOGGLE_508_OFF'] ?>";
-		let toggleOn508 = "<?= $LANG['F_TOGGLE_508_ON'] ?>";
-	</script>
-	 
-	<script src="<?= $CLIENT_ROOT; ?>/js/symb/accessibility.footer.js?ver=1" type="text/javascript"></script> 
-	--->
 </footer>
