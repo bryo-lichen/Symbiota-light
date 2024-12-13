196e73966fd38cdd1d95337abf28f6d682da5e95
Katie Pearson <katelin.d.pearson24@gmail.com>
2024-12-13 15:40:44 -0800

    Update header and footer

--- includes/footer.php
+++ includes/footer.php
@@ -6,16 +6,20 @@
 		<a href="https://biokic.asu.edu" target="_blank" title="<?= $LANG['F_BIOKIC'] ?>" aria-label="Visit BioKIC website">
 			<img src="<?= $CLIENT_ROOT; ?>/images/layout/logo-asu-biokic.png"  alt="<?= $LANG['F_BIOKIC_LOGO'] ?>" />
 		</a>
+		<a href="https://biodiversity.ku.edu/" target="_blank" aria-label="Visit KU BI website">
+			<img src="<?= $CLIENT_ROOT; ?>/images/layout/KU_BI.PNG" alt="KU BI logo" />
+		</a>
 		<a href="http://idigbio.org" target="_blank" title="iDigBio" aria-label="<?= $LANG['F_VISIT_IDIGBIO'] ?>">
 			<img src="<?= $CLIENT_ROOT; ?>/images/layout/logo_idig.png" alt="<?= $LANG['F_IDIGBIO_LOGO'] ?>" />
 		</a>
-		<a href="https://www.nsf.gov" target="_blank" aria-label="<?= $LANG['F_VISIT_NSF'] ?>">
-			<img src="<?= $CLIENT_ROOT; ?>/images/layout/logo_nsf.gif" alt="<?= $LANG['F_NSF_LOGO'] ?>" />
-		</a>
-		<a href="https://globaltcn.utk.edu/" targe="_blank">
-			<img src="<?php echo $CLIENT_ROOT; ?>/images/layout/global_logo.png" style="width:150px" />
+		<a href="https://globaltcn.utk.edu/" target="_blank">
+			<img src="<?php echo $CLIENT_ROOT; ?>/images/layout/global_logo.png" />
 		</a>
 	</div>
+	<a href="https://www.nsf.gov" target="_blank" aria-label="<?= $LANG['F_VISIT_NSF']?>">
+			<img src="<?= $CLIENT_ROOT; ?>/images/layout/logo_nsf.gif" alt="<?= $LANG['F_NSF_LOGO'] ?>" />
+	</a>
+	<br></br>
 	<p>
 		This project made possible by U.S. National Science Foundation Awards:
 		<a href="https://www.nsf.gov/awardsearch/showAward?AWD_ID=1115116" target="_blank">#1115116</a>,
