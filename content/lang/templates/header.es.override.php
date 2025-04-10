8f5fba472f8d8a6894fea37ba62f71160d9be60a
Katie Pearson <katelin.d.pearson24@gmail.com>
2025-04-10 11:32:16 -0700

    Update header.es.override.php

--- content/lang/templates/header.es.override.php
+++ content/lang/templates/header.es.override.php
@@ -20,7 +20,7 @@ $LANG['GLOBAL_NETWORK'] = 'Red GLOBAL de Briofitas y Líquenes';
 $LANG['H_HELP_RESOURCES'] = 'Ayuda y Recursos';
 $LANG['CONSORTIUM_RESOURCES'] = 'Recursos del Consorcio';
 $LANG['GLOSSARY'] = 'Glosario';
-$LANG['Map'] = 'Búsqueda de Mapas';
+$LANG['H_MAP'] = 'Búsqueda de Mapas';
 
 
 ?>
