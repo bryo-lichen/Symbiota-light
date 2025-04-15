b49d862f4a1fd947e979965431d5db24dba4b27f
Katie Pearson <katelin.d.pearson24@gmail.com>
2025-04-15 11:18:16 -0700

    Update homepage text and fix usagepolicy

--- index.php
+++ index.php
@@ -58,43 +58,51 @@ header('Content-Type: text/html; charset=' . $CHARSET);
 		if($LANG_TAG=='es'){
 		?>
 			<p>
-				El <b>Consorcio de Herbarios de Bri&oacute;fitas</b> sirve como una puerta de entrada a la biodiversidad de musgos,
-				hep&aacute;ticas y antocerotes en el mundo. El Consorcio incluye registros de espec&iacute;menes de bri&oacute;fitas en
-				colecciones institucionales y personales. Nuestro objetivo es compartir estos registros p&uacute;blicamente
-				con usuarios en todo el mundo. Originalmente el Consorcio empez&oacute; con la digitalizaci&oacute;n de herbarios en
-				Am&eacute;rica del Norte, pero ahora invitamos a todos los colecciones del mundo a unirse al Consorcio para
-				compartir sus registros m&aacute;s ampliamente con la comunidad de cient&iacute;fica internacional. Actualmente
-				ofrecemos estos datos a trav&eacute;s de una interfaz en ingl&eacute;s y espa&ntilde;ol; una versi&oacute;n
-				en franc&eacute;s est&acute; en desarrollo.
+				El <b>Consorcio de Herbarios de Briófitas</b> sirve como una puerta de entrada a la biodiversidad de musgos, hepáticas y antocerotes en el mundo. 
+				El Consorcio incluye registros de especímenes de briófitas en colecciones institucionales y personales. Nuestro objetivo es compartir estos 
+				registros públicamente con usuarios en todo el mundo. Originalmente el Consorcio empezó con la digitalización de herbarios en América del Norte, 
+				pero ahora invitamos a todos los colecciones del mundo a unirse al Consorcio para compartir sus registros más ampliamente con la comunidad 
+				de científica internacional.
 			</p>
 			<p>
-				Ofrecemos herramientas para ubicar, acceder y trabajar con una variedad de datos, especialmente registros
-				de espec&iacute;menes, observaciones de campo, listas de especies est&aacute;ticas y din&aacute;micas, im&aacute;genes, claves
-				interactivas y un tesauro taxon&oacute;mico. Si usted visita este sitio por la primera vez le sugerimos <a href="profile/newprofile.php">crear
-				una cuenta</a>. Si est&aacute; interesado en contribuir y necesita ayuda como usuario individual o administrador
-				de una instituci&oacute;n, estamos disponibles en el correo:
-				<a class="bodylink" href="BryophyteConsortium@gmail.com">BryophyteConsortium@gmail.com</a>. Las cuentas creadas
-				previamente en el Consorcio de Norte Am&eacute;rica seguir&aacute;n funcionando.
+				Ofrecemos herramientas para ubicar, acceder y trabajar con una variedad de datos, especialmente registros de especímenes, 
+				observaciones de campo, listas de especies estáticas y dinámicas, imágenes, claves interactivas y un tesauro taxonómico. 
+				Si usted visita este sitio por la primera vez le sugerimos <a href="profile/newprofile.php">crear
+				una cuenta</a>. Si está interesado en contribuir y necesita ayuda como usuario individual o administrador de una institución, 
+				estamos disponibles en el correo: <a class="bodylink" href="BryophyteConsortium@gmail.com">BryophyteConsortium@gmail.com</a>.
+			</p>
+		<?php
+		} else if($LANG_TAG=='fr'){
+		?>
+			<p>
+				Le <b>Consortium des Herbiers de Bryophytes</b> sert de passerelle vers les données sur la biodiversité végétale des mousses, 
+				des hépatiques et des anthocérotes. Son objectif est de rassembler les données sur les spécimens de bryophytes du monde entier, 
+				incluant les collections personnelles et les observations de recherche, et de servir de passerelle pour la diffusion de ces 
+				ressources auprès du public. Initialement axé sur les herbiers nord-américains, le Consortium invite désormais tous les herbiers 
+				à le rejoindre afin de partager leurs données avec la communauté scientifique internationale via cette plateforme.
+			</p>
+			<p>
+				Nous proposons des outils permettant de localiser, d'accéder et d'exploiter une variété de données, notamment des fiches de spécimens, 
+				des observations de terrain, des listes de contrôle dynamiques et statiques, des images, des clés interactives et un thésaurus taxonomique. 
+				Si vous êtes nouveau sur le site, <a href="profile/newprofile.php">veuillez créer un compte</a>. Contactez-nous si vous souhaitez contribuer, 
+				que ce soit en tant qu'utilisateur individuel ou en tant que gestionnaire de collection d'une institution souhaitant nous rejoindre: 
+				<a class="bodylink" href="BryophyteConsortium@gmail.com">BryophyteConsortium@gmail.com</a>.
 			</p>
 		<?php
 		} else {
 		?>
 			<p>
-				The <b>Consortium of Bryophyte Herbaria</b> serves as gateway to plant biodiversity data for mosses,
-				liverworts, and hornworts. The aim of the Consortium is to unite bryophyte specimen records from
-				around the world, including personal collections and research observations, and serve as a gateway
-				to distribute these resources to the public. The Consortium began with a focus on North American herbaria.
-				It now welcomes all herbaria to join the Consortium to share specimen records with the international
-				research community via this platform. We currently serve the data through an English and Spanish language
-				interface; a French version is in development.
+				The <b>Consortium of Bryophyte Herbaria</b> serves as gateway to plant biodiversity data for mosses, liverworts, and hornworts. 
+				The aim of the Consortium is to unite bryophyte specimen records from around the world, including personal collections and research observations, 
+				and serve as a gateway to distribute these resources to the public. The Consortium began with a focus on North American herbaria. 
+				It now welcomes all herbaria to join the Consortium to share specimen records with the international research community via this platform.
 			</p>
 			<p>
-				We offer tools to locate, access and work with a variety of data, including specimen records, field observations,
-				dynamic and static checklists, images, interactive keys, and a taxonomic thesaurus. If you are new to the site,
-				please <a href="profile/newprofile.php">create an account</a>. Contact us if you are interested in contributing,
+				We offer tools to locate, access and work with a variety of data, including specimen records, field observations, 
+				dynamic and static checklists, images, interactive keys, and a taxonomic thesaurus. If you are new to the site, 
+				please <a href="profile/newprofile.php">create an account</a>. Contact us if you are interested in contributing, 
 				either as individual user or as a collection manager of an institution that would like to join:
 				<a class="bodylink" href="BryophyteConsortium@gmail.com">BryophyteConsortium@gmail.com</a>.
-				Previously-created accounts in CNABH will continue to function.
 			</p>
 		<?php
 		}
